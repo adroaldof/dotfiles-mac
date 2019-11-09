6717529a12ce8d2f29a594d34aa286bd0896fb0e
Adroaldo de Andrade <adroaldof@gmail.com>
2019-11-09 17:33:56 -0300

    Add neovim to dotfiles setup

--- setup-dotfiles.sh
+++ setup-dotfiles.sh
@@ -10,45 +10,43 @@
 #     echo "We found Tmux Plugin Manager"
 # fi
 
-
 echo "###############################################################################"
 echo "# Create All Symlinks to Dotfiles"
 echo "###############################################################################"
-rm -rf ~/.aliases > /dev/null 2>&1
-rm -rf ~/.aliases.sh > /dev/null 2>&1
-rm -rf ~/.gitconfig > /dev/null 2>&1
-rm -rf ~/.npmrc > /dev/null 2>&1
-rm -rf ~/.tmux.conf > /dev/null 2>&1
-rm -rf ~/.vim > /dev/null 2>&1
-rm -rf ~/.vimrc > /dev/null 2>&1
-rm -rf ~/.zshrc > /dev/null 2>&1
+rm -rf ~/.aliases >/dev/null 2>&1
+rm -rf ~/.aliases.sh >/dev/null 2>&1
+rm -rf ~/.gitconfig >/dev/null 2>&1
+rm -rf ~/.npmrc >/dev/null 2>&1
+rm -rf ~/.tmux.conf >/dev/null 2>&1
+rm -rf ~/.config/nvim >/dev/null 2>&1
+rm -rf ~/.vim >/dev/null 2>&1
+rm -rf ~/.vimrc >/dev/null 2>&1
+rm -rf ~/.zshrc >/dev/null 2>&1
 
 ln -s ~/dotfiles-mac/aliases ~/.aliases
 ln -s ~/dotfiles-mac/aliases.sh ~/.aliases.sh
 ln -s ~/dotfiles-mac/gitconfig ~/.gitconfig
 ln -s ~/dotfiles-mac/npmrc ~/.npmrc
+ln -s ~/dotfiles-mac/zshrc ~/.zshrc
+ln -s ~/dotfiles-mac/nvim ~/.config/nvim
 # ln -s ~/dotfiles-mac/tmux ~/.tmux
 # ln -s ~/dotfiles-mac/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles-mac/vim ~/.vim
-ln -s ~/dotfiles-mac/vimrc ~/.vimrc
-ln -s ~/dotfiles-mac/zshrc ~/.zshrc
-
+# ln -s ~/dotfiles-mac/vim ~/.vim
+# ln -s ~/dotfiles-mac/vimrc ~/.vimrc
 
 echo "###############################################################################"
 echo "# Install on-my-zsh theme"
 echo "###############################################################################"
-rm -rf ~/.oh-my-zsh/themes/easy-look.zsh-theme > /dev/null 2>&1
-rm -rf ~/.oh-my-zsh/themes/agnoster-improved.zsh-theme > /dev/null 2>&1
+rm -rf ~/.oh-my-zsh/themes/easy-look.zsh-theme >/dev/null 2>&1
+rm -rf ~/.oh-my-zsh/themes/agnoster-improved.zsh-theme >/dev/null 2>&1
 
 ln -s ~/dotfiles-mac/easy-look.zsh-theme ~/.oh-my-zsh/themes/easy-look.zsh-theme
 ln -s ~/dotfiles-mac/agnoster-improved.zsh-theme ~/.oh-my-zsh/themes/agnoster-improved.zsh-theme
-
 
 echo "###############################################################################"
 echo "# Install Fonts"
 echo "###############################################################################"
 cp ~/dotfiles-mac/fonts/* /Library/Fonts
-
 
 echo "###############################################################################"
 echo "# Check for VundleVim Installation"
@@ -61,15 +59,12 @@ else
   echo "We found VundleVim Package Manager"
 fi
 
-
 echo "###############################################################################"
 echo "# Install Vundle Bundles"
 echo "###############################################################################"
-vim +BundleInstall! +qall > /dev/null 2>&1
-
+vim +BundleInstall! +qall >/dev/null 2>&1
 
 echo "###############################################################################"
 echo "# Reload ZSH"
 echo "###############################################################################"
 source ~/.zshrc
-
